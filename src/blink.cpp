d4ce0abd8c53c6e3661352d6ea90ef85bb372a5a
digitalhabitat <miketm89@gmail.com>
2023-12-17 23:55:11 -0500

    add ros2 topic blink1 control test

--- src/blink.cpp
+++ src/blink.cpp
@@ -13,31 +13,68 @@
 // limitations under the License.
 
 #include <cstdlib>  // For system function
+#include <sys/wait.h> // waitpid()
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class blink1InterfaceNode : public rclcpp::Node
+{
+public:
+  blink1InterfaceNode()
+  : Node("blink1_interface_node")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>
+    ("string_topic", 10, std::bind(&blink1InterfaceNode::callback, this, _1));
+  }
+
+  void launchTinyServer()
+  {
+    std::string filepath = "/workspaces/bot2_ros2_workspace/src2/blink1-tool/blink1-tiny-server";
+    std::string filename = "blink1-tiny-server";
+    
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+      std::perror("Error forking process");
+      std::exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+      // Child process
+      execlp(filepath.c_str(), filename.c_str(), "--port", "8123", "-no-html", nullptr);
+      // If execlp fails
+      std::perror("Error executing child process");
+      std::string errmsg = "Executing child process: " + filename + "failed. Shutting"; 
+      std::exit(EXIT_FAILURE);
+      RCLCPP_ERROR(get_logger(), "Operation failed. Shutting down.");
+      rclcpp::shutdown();  // Terminate the node
+    }
+  }
+
+private:
+  void callback(const std_msgs::msg::String & cmd) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'",cmd.data.c_str());
+    std::string uri = "curl localhost:8123/blink1/" + cmd.data;
+    std::system(uri.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("simple_node");
-
-  const char * command_on = " blink1-tool --rgb=ffdf00  --blink=0 --led=1";
-  const char * command_off = "blink1-tool --off";
-
-  int result = system(command_on);
-  if (result != 0) {
-    std::cerr << "Command failed to execute." << std::endl;
-  }
-
+  auto node = std::make_shared<blink1InterfaceNode>();
+  node->launchTinyServer();
+ 
   rclcpp::spin(node);
 
   rclcpp::shutdown();
-
-  result = system(command_off);
-  if (result != 0) {
-    std::cerr << "Command failed to execute." << std::endl;
-  }
 
   return 0;
 }
