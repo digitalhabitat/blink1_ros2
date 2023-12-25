be2777c587525f6442c34fcb8c8191d01a7e96d6
digitalhabitat <miketm89@gmail.com>
2023-12-25 20:49:57 +0000

    fix: include basic launch file

---
+++ launch/blink1_launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='blink1_ros2',
+            executable='blink',
+        )
+    ])
